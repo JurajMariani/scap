6b6ef4832e64545587f6336960532526d5f1c0d1
Juraj Mariani <juraj2.mariani@gmail.com>
2025-05-02 19:25:59 +0200

    Implemented block validation logic

--- src/blockchain/block.py
+++ src/blockchain/block.py
@@ -1,11 +1,14 @@
-import rlp
+from rlp import Serializable, encode, decode
 from rlp.sedes import big_endian_int, Binary, binary, CountableList
 from eth_keys import keys
 from eth_utils import keccak
+from trie import HexaryTrie
+from copy import deepcopy
 
 from transaction import Transaction, TxSerializable
+from state import StateTrie
 
-class BlockFull(rlp.Serializable):
+class BlockSerializable(Serializable):
     fields = [
         ('parent_hash', Binary.fixed_length(32, allow_empty=False)),
         ('state_root', Binary.fixed_length(32, allow_empty=False)),
@@ -14,12 +17,105 @@ class BlockFull(rlp.Serializable):
         ('block_number', big_endian_int),
         ('beneficiary', Binary.fixed_length(20, allow_empty=False)),
         ('timestamp', big_endian_int),
-        ('signature', Binary.fixed_length(65, allow_empty=False)),
-        ('data', Binary.fixed_length(32, allow_empty=True)),
+        ('sig_v', big_endian_int),
+        ('sig_r', big_endian_int),
+        ('sig_s', big_endian_int),
+        ('data', binary),
         ('transactions', CountableList(TxSerializable))
     ]
 
-class BlockNoSig(rlp.Serializable):
+    def recoverAddress(self):
+        signature = keys.Signature(vrs=(self.sig_v, self.sig_r, self.sig_s))
+        pk = signature.recover_public_key_from_msg_hash(self.rebuildHash())
+        return pk.to_canonical_address()
+    
+    def rebuildHash(self):
+        blockns = BlockNoSig(
+            self.parent_hash,
+            self.state_root,
+            self.transasction_root,
+            self.receipts_root,
+            self.block_number,
+            self.beneficiary,
+            self.timestamp,
+            self.data
+        )
+        return keccak(encode(blockns))
+    
+    def calculateTxHash(self) -> binary:
+        hashlist = []
+        for tx in self.transactions:
+            hashlist.append(tx.hash())
+        i = 0
+        while (i < len(hashlist)):
+            if (i + 1 >= len(hashlist)):
+                return hashlist[-1]
+            nhash = hashlist[i] + hashlist[i + 1]
+            hashlist.append(keccak(nhash))
+            i += 2
+        return b''
+    
+    def verifyBlock(self, state: StateTrie, parentH: binary, parentBlockNo: int) -> tuple[StateTrie, bool]:
+        # Verify block signature
+        if (not self.verifySig(self.beneficiary)):
+            return (state, False)
+        # Get beneficiary Account from state
+        benef = state.getAccount(self.beneficiary)
+        # Verify Beneficiary's existance
+        if (not benef):
+            return (state, False)
+        # Only a creator can be a consensus node, therefore, they must exist
+        if (not benef.isConsensusNode()):
+            return (state, False)
+        # Verify TX root
+        if (self.transactions_root != self.calculateTxHash()):
+            return (state, False)
+        # Verify Transactions
+        if (not self.verifyTXs(state)):
+            return (state, False)
+        # Verify parent hash
+        if (not self.parent_hash != parentH):
+            return (state, False)
+        # Verify block number
+        if (not self.block_number != parentBlockNo + 1):
+            return (state, False)
+        # Verify state root validity
+        return self.verifyStateAfterExecution(state)
+
+
+    def verifySig(self, acc: binary) -> bool:
+        return (self.recoverAddress() == acc)
+    
+    def verifyTXs(self, state: StateTrie) -> bool:
+        for tx in self.transactions:
+            if (not state.transaction(tx, True, False)):
+                return False
+        return True
+    
+    def verifyStateAfterExecution(self, state: StateTrie) -> tuple[StateTrie, bool]:
+        '''
+        Returns the correct state of the blockchain.
+        The validity of the block can be inferred from the boolean value.
+        '''
+        # 1. deepcopy state to a tmp variable
+        tmp = StateTrie()
+        tmp.db = deepcopy(state.db)
+        tmp.iddb = deepcopy(state.iddb)
+        tmp.state_trie = HexaryTrie(tmp.db, root_hash=state.getRootHash())
+        tmp.id_trie = HexaryTrie(tmp.iddb, root_hash=state.id_trie.root_hash)
+        # 2. On the tmp state, noverify, execute all txs
+        for tx in self.transactions:
+            tmp.transaction(tx, False, True)
+        # 3. Verify state roothash
+        if (self.state_root != tmp.getRootHash()):
+            # 4. IF NONMATCHING ROOTHASH
+            #   5. Discard Changes (new final state is orig)
+            return (state, False)
+        # 4. IF MATCHING ROOTHASH
+        #   5. Apply Changes (new final state is tmp)
+        return (tmp, True)
+
+class BlockNoSig(Serializable):
     fields = [
         ('parent_hash', Binary.fixed_length(32, allow_empty=False)),
         ('state_root', Binary.fixed_length(32, allow_empty=False)),
@@ -29,11 +125,11 @@ class BlockNoSig(rlp.Serializable):
         ('beneficiary', Binary.fixed_length(20, allow_empty=False)),
         ('timestamp', big_endian_int),
         #('signature', Binary.fixed_length(65, allow_empty=False)),
-        ('data', Binary.fixed_length(32, allow_empty=True)),
+        ('data', binary),
         #('transactions', CountableList(Transaction))
     ]
 
-class BlockSig(rlp.Serializable):
+class BlockSig(Serializable):
     fields = [
         ('parent_hash', Binary.fixed_length(32, allow_empty=False)),
         ('state_root', Binary.fixed_length(32, allow_empty=False)),
@@ -42,8 +138,10 @@ class BlockSig(rlp.Serializable):
         ('block_number', big_endian_int),
         ('beneficiary', Binary.fixed_length(20, allow_empty=False)),
         ('timestamp', big_endian_int),
-        ('signature', Binary.fixed_length(65, allow_empty=False)),
-        ('data', Binary.fixed_length(32, allow_empty=True)),
+        ('sig_v', big_endian_int),
+        ('sig_r', big_endian_int),
+        ('sig_s', big_endian_int),
+        ('data', binary),
         #('transactions', CountableList(Transaction))
     ]
 
@@ -65,30 +163,14 @@ class Block():
         self.transactions.append(tx)
 
     def serializeNoSig(self):
-        return rlp.encode(BlockNoSig(self.parent_hash, self.state_root, self.transactions_root, self.receipts_root, self.block_number, self.beneficiary, self.timestamp, self.data))
+        return encode(BlockNoSig(self.parent_hash, self.state_root, self.transactions_root, self.receipts_root, self.block_number, self.beneficiary, self.timestamp, self.data))
 
     def serializeSig(self):
-        return rlp.encode(BlockSig(self.parent_hash, self.state_root, self.transactions_root, self.receipts_root, self.block_number, self.beneficiary, self.timestamp, self.signature.to_bytes(), self.data))
+        return encode(BlockSig(self.parent_hash, self.state_root, self.transactions_root, self.receipts_root, self.block_number, self.beneficiary, self.timestamp, self.signature.to_bytes(), self.data))
 
     def serialize(self):
         print(self.signature.to_bytes().hex())
-        return rlp.encode(BlockFull(self.parent_hash, self.state_root, self.transactions_root, self.receipts_root, self.block_number, self.beneficiary, self.timestamp, self.signature.to_bytes(), self.data, []))
-    
-    def calculateTxHash(self):
-        hashlist = []
-        for tx in self.transactions:
-            if not tx.hash:
-                tx.hashTx()
-            hashlist.append(tx.hash)
-        i = 0
-        while (i < len(hashlist)):
-            if (i + 1 >= len(hashlist)):
-                self.transactions_root = hashlist[-1]
-                return 0
-            nhash = hashlist[i] + hashlist[i + 1]
-            hashlist.append(keccak(nhash))
-            i += 2
-        return -1
+        return encode(BlockSerializable(self.parent_hash, self.state_root, self.transactions_root, self.receipts_root, self.block_number, self.beneficiary, self.timestamp, self.signature.to_bytes(), self.data, []))
     
     def serializeTxs(self):
         txs = []
