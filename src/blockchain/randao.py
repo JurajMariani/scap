e6595356b0eb406b8d61d6bb57b1aed0e35057bd
Juraj Mariani <juraj2.mariani@gmail.com>
2025-05-02 22:45:03 +0200

    FIX: randao mechanism

--- src/blockchain/randao.py
+++ src/blockchain/randao.py
@@ -1,46 +1,19 @@
-import hashlib
 import random
 import time
 
 class Randao:
     def __init__(self):
-        self.commitments = {}
-        self.reveals = {}
-        self.stale = False
+        self.seed = b'\x00'
 
-    def commit(self, validator_id, commit_value):
-        """Validators commit to a secret value"""
-        self.commitments[validator_id] = commit_value
+    def get_seed(self) -> bytes:
+        return self.seed
+    
+    def reseed(self, seed: bytes) -> None:
+        self.seed = seed
+        random.seed(seed)
 
-    def reveal(self, validator_id, secret_value):
-        """Validators reveal their secret value after committing"""
-        if validator_id not in self.commitments:
-            raise ValueError(f"Validator {validator_id} has not committed yet.")
-        
-        commit_value = self.commitments[validator_id]
-        self.stale = False
-        
-        # Reveal phase
-        if hashlib.sha256(secret_value.encode()).hexdigest() == commit_value:
-            self.reveals[validator_id] = secret_value
-        else:
-            raise ValueError(f"Revealed value does not match the commitment for {validator_id}.")
-        
-    def commit_and_reveal(self, validator_id, secret_value):
-        """Simulate the commit and reveal phases of Randao."""
-        self.commit(validator_id, hashlib.sha256(secret_value.encode()).hexdigest())
-        self.reveal(validator_id, secret_value)
-        
-    def get_random_value(self):
-        """Generate randomness from the reveals"""
-        if (self.stale):
-            raise ValueError('Random value is stale.')
-        combined_reveals = ''.join([self.reveals[v] for v in self.reveals])
-        if not combined_reveals:
-            raise ValueError("No reveals have been made yet.")
-        self.stale = True
-        # Wipe commits and reveals
-        self.commitments = {}
-        self.reveals = {}
-        # Hash the combined reveals to create randomness
-        return hashlib.sha256(combined_reveals.encode()).hexdigest()
+    def getValue() -> float:
+        return random.random()
+    
+    def getValueInRange(low: int, high: int) -> int:
+        return random.randint(low, high)
