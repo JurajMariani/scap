636f6c209e50d6af437934c1d712803a0b95470b
Juraj Mariani <juraj2.mariani@gmail.com>
2025-05-03 00:40:01 +0200

    ADD: shuffleList

--- src/blockchain/randao.py
+++ src/blockchain/randao.py
@@ -4,16 +4,21 @@ import time
 class Randao:
     def __init__(self):
         self.seed = b'\x00'
+        self.rng = random.Random(self.seed)
 
     def get_seed(self) -> bytes:
         return self.seed
     
     def reseed(self, seed: bytes) -> None:
         self.seed = seed
-        random.seed(seed)
+        self.rng.seed(seed)
 
-    def getValue() -> float:
-        return random.random()
+    def getValue(self) -> float:
+        return self.rng.random()
     
-    def getValueInRange(low: int, high: int) -> int:
-        return random.randint(low, high)
+    def getValueInRange(self, low: int, high: int) -> int:
+        return self.rng.randint(low, high)
+    
+    def shuffleList(self, llist: list) -> list:
+        self.rng.shuffle(llist)
+        return llist
